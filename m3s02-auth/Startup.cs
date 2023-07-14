d62fac3a2ede8ba48a0f6a28f7e2b32d1248e967
Vitor Lassen <vitor@lassen.com.br>
2023-07-13 23:24:24 -0300

    add headers on swagger

--- m3s02-auth/Startup.cs
+++ m3s02-auth/Startup.cs
@@ -77,6 +77,30 @@ namespace m3s02_auth
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "m3s02_auth", Version = "v1" });
+                c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. 
+                                              Escreva 'Bearer' [espaço] e o token gerado no login na caixa abaixo.
+                                              Exemplo: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                                          {
+                                            {
+                                              new OpenApiSecurityScheme
+                                              {
+                                                Reference = new OpenApiReference
+                                                  {
+                                                    Type = ReferenceType.SecurityScheme,
+                                                    Id = JwtBearerDefaults.AuthenticationScheme
+                                                  },
+                                                },
+                                                new List<string>()
+                                              }
+                                            });
             });
         }
 
